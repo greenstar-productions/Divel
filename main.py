ead4ec25ae366bfa4df37985240ba8e0c681f083
haovipaws <darkclawsthenightwing@gmail.com>
2020-09-22 16:35:07 -0700

    add player things

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
+import player.py
 
 # Initialization
 pygame.init()
 
-window = pygame.display.set_mode((500, 500))
+window = pygame.display.set_mode((500, 250))
 
 pygame.display.set_caption("Divel, a dungeon crawler written in python")
 
+class player:
+    class main_player:
+        x = 250
+        y = 125
 
+#while game_running:
+#   blah blah code stuff
+
+# De-Initialization
+pygame.quit()
---
+++ player.py
@@ -0,0 +1,17 @@
+import pygame
+
+class player:
+    class main_player:
+        x = 250
+        y = 125
+    class other_player:
+        def _init(x, y):
+        def move(int):
+            if int = 1:
+                x -= 1
+            if int = 2:
+                y -= 1
+            if int = 3:
+                x += 1
+            if int = 4:
+                y += 1
