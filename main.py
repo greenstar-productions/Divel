792a87426728c0917daf799c663e817f80e51ea2
haovipaws <darkclawsthenightwing@gmail.com>
2020-10-01 17:44:29 -0700

    add gitignore

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
+import sys
 import player
 
 # Initialization
 pygame.init()
-
-window = pygame.display.set_mode((1500, 1250))
+clock = pygame.time.Clock()
+window = pygame.display.set_mode((1500, 800))
 
 pygame.display.set_caption("Divel, a dungeon crawler written in python")
 
-while 1:
+game = True
+while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            break
+            game == False
+            sys.exit()
 
 # De-Initialization
 pygame.quit()
