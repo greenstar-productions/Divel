440d506cb4105eb2ebe96a315ccea5fbcbf1a181
haovipaws <darkclawsthenightwing@gmail.com>
2020-10-03 16:52:50 -0700

    add font

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ window = pygame.display.set_mode((1500, 800))
 pygame.display.set_caption("Divel, a dungeon crawler written in python")
 
 game = True
-while game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game == False
-            sys.exit()
+def main_menu():
 
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game == False
+                sys.exit()
+
+main_menu()
 # De-Initialization
 pygame.quit()
