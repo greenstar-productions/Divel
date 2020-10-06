b9bbb9ea81e6a407db08e57a56df336074381a65
oregone1 <54779457+oregone1@users.noreply.github.com>
2020-10-06 00:20:45 -0700

    de-spaghettification
    made code easier to read

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ import player
 pygame.init()
 clock = pygame.time.Clock()
 window = pygame.display.set_mode((1500, 800), pygame.RESIZABLE)
-
 font = pygame.font.Font("undefined-medium.ttf", 20)
+pygame.display.set_caption("Divel, a dungeon crawler written in python")
+
+# variables
+game = True
+click = False
 
 def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
- 
-click = False
 
-pygame.display.set_caption("Divel, a dungeon crawler written in python")
-
-game = True
 def main_menu():
     while game:
-        # button stuff ane things
+        # button stuff and things
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game == False
                 sys.exit()
+
 
 # De-Initialization
     pygame.quit()
