9730fc4ce3f993ad181a5e6301f42d8d2118b904
haovipaws <darkclawsthenightwing@gmail.com>
2020-10-05 20:45:54 -0700

    use pixel art font, which is more fitting to the game

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ import player
 # Initialization
 pygame.init()
 clock = pygame.time.Clock()
-window = pygame.display.set_mode((1500, 800))
-font = pygame.font.Font("Inter-Thin.otf", 15)
+window = pygame.display.set_mode((1500, 800), pygame.RESIZABLE)
+
+font = pygame.font.Font("undefined-medium.ttf", 20)
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+ 
+click = False
 
 pygame.display.set_caption("Divel, a dungeon crawler written in python")
 
