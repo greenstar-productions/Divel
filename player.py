cf1079b60fa8da114650dd565113c50f96303a3d
haovipaws <darkclawsthenightwing@gmail.com>
2020-10-08 17:42:42 -0700

    move code to where its supposed to be

--- player.py
+++ player.py
@@ -4,9 +4,6 @@ class player:
     class main_player:
         x = 250
         y = 125
-    class other_player:
-        def __init__(x, y):
-            raise
         def move(button):
             if button == 1:
                 x -= 1
@@ -16,3 +13,6 @@ class player:
                 x += 1
             if button == 4:
                 y += 1
+    class other_player:
+        def __init__(x, y):
+            raise
